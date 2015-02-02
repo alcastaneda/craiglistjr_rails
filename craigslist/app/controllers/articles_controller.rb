a630e8d5228ee07f6770391e0050a1d3d57b50c0
Adriana & Dalal Alrayes <pair+alcastaneda.dalrayes@devbootcamp.com>
2015-02-02 17:54:41 -0600

    Craigs list railsified - needs CSS love

--- craigslist/app/controllers/articles_controller.rb
+++ craigslist/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+
   def show
     @article = Article.find(params[:id])
   end
@@ -6,6 +7,55 @@ class ArticlesController < ApplicationController
   def new
     @category_options = Category.all.map{|c| [ c.title, c.id ] }
     @article = Article.new
+  end
 
+  def create
+
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.key = create_key
+      @article.save
+    end
+    redirect_to "/articles/#{@article.id}/success"
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+      if @article.key == params[:key]
+       @category_options = Category.all.map{|c| [ c.title, c.id ] }
+       edit_article_path(@article)
+
+      else
+        redirect_to article_path(@article)
+      end
+  end
+
+  def update
+    @article= Article.find(params[:id])
+    @article.update_attributes(article_params)
+    @category = @article.category
+    redirect_to category_path(@category)
+  end
+
+  def destroy
+    @article= Article.find(params[:id])
+    @category = @article.category
+    @article.destroy
+    redirect_to category_path(@category)
+  end
+
+  def success
+    @article = Article.find(params[:id])
+  end
+
+   private
+
+   def article_params
+      params.require(:article).permit(:title, :description, :price, :category_id)
+   end
+
+  def create_key
+    @key = Faker::Internet.password(8)
+    return @key
   end
 end
--- craigslist/config/routes.rb
+++ craigslist/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # get '/' => "application#index"
+  get '/articles/:id/success' => 'articles#success'
+  get '/articles/:id/:key' => 'articles#edit'
+
   resources :articles, :categories
   root to: "categories#index"
 end
